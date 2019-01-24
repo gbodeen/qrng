fb5f6683da61bdd151539a6b98761421d0b8a6cf
Gabriel Bodeen <gabebodeen@gmail.com>
2019-01-24 17:08:55 -0600

    Initial commit: Set up React/Webpack/Babel/Express.

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <>
+        <h1>The app will go here.</h1>
+        <h2>But it hasn't been written yet.</h2>
+      </>
+    )
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  entry: __dirname + '/src/index.jsx',
+  output: {
+    path: __dirname + '/public/',
+    filename: 'lotto.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  watch: true,
+  mode: 'development'
+};
