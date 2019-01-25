1453334043f63456d9168f8e4aa80ecb1cdde1ae
Gabriel Bodeen <gabebodeen@gmail.com>
2019-01-24 19:13:32 -0600

    Update to react@next and react-dom@next to use new hooks.

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      bits: ''
-    }
-  }
+const App = () => {
+  const [bits, setBits] = useState('');
 
-  getBits = () => {
+  const getBits = () => {
     fetch('https://qrng.anu.edu.au/API/jsonI.php?length=8&type=uint16',
       { method: 'GET', mode: 'cors' })
       .then(response => response.json())
       .then(json => {
-        let bits = json.data.map(n => n.toString(2)).join('') + this.state.bits;
-        this.setState({ bits },
-          () => console.log('supply: ', this.state.bits))
+        bits = json.data.map(n => n.toString(2)).join('') + bits;
+        setBits(bits);
       })
   }
 
-  render() {
-    return (
-      <>
-        <h1>The app will go here.</h1>
-        <h2>But it hasn't been written yet.</h2>
-      </>
-    )
-  }
+  return (
+    <>
+      <h1>The app will go here.</h1>
+      <h2>But it hasn't been written yet.</h2>
+    </>
+  )
 }
 
 export default App;
